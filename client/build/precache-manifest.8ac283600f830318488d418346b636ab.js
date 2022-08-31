eb96ae3830ae5afe13184bdbac7fd6415f34b16a
C K <ctkoo@ucsc.edu>
2022-08-31 02:42:43 -0700

    working

---
+++ client/build/precache-manifest.8ac283600f830318488d418346b636ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a326005bf0bd8646bc1a9e23de6a3c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7351d4981a8f2d5edf1f",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "38cc616f8699f3890881",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "7351d4981a8f2d5edf1f",
+    "url": "/static/js/2.3945a21c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3945a21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38cc616f8699f3890881",
+    "url": "/static/js/main.c4db108c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c4db108c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
